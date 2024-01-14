77c6297c673fac10c1d18de79aa84221d7cffbc9
Fiifi Pius <fiifipius@gmail.com>
2024-01-14 18:44:48 +0000

    added subjects

--- app/Http/Actions/Assessment/ViewSBAAction.php
+++ app/Http/Actions/Assessment/ViewSBAAction.php
@@ -4,8 +4,10 @@ namespace App\Http\Actions\Assessment;
 
 use App\Http\Resources\LevelResource;
 use App\Http\Resources\StudentResource;
+use App\Http\Resources\SubjectResource;
 use App\Models\Level;
 use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,6 +18,7 @@ class ViewSBAAction
     {
         return Inertia::render('Assessment/SBAView', [
             'levels' => LevelResource::collection(Level::all()),
+            'subjects' => SubjectResource::collection(Subject::with('levels')->get()),
             'students' => StudentResource::collection(Student::where('form', $request->level)
                 ->where('status', Student::ACTIVE)->orderBy('name', 'ASC')->get()),
         ]);
