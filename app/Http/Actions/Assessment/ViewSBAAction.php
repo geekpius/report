3e9229fb5cb0981d0d32a626f0427d22db256e04
Fiifi Pius <fiifipius@gmail.com>
2024-01-16 09:16:57 +0000

    fetch students with their subject mark

--- app/Http/Actions/Assessment/ViewSBAAction.php
+++ app/Http/Actions/Assessment/ViewSBAAction.php
@@ -3,8 +3,10 @@
 namespace App\Http\Actions\Assessment;
 
 use App\Http\Resources\LevelResource;
+use App\Http\Resources\StudentMarkResource;
 use App\Http\Resources\StudentResource;
 use App\Http\Resources\SubjectResource;
+use App\Models\Academic;
 use App\Models\Level;
 use App\Models\Student;
 use App\Models\Subject;
@@ -16,11 +18,22 @@ class ViewSBAAction
 {
     public function handle(Request $request): Response
     {
+        $academic = Academic::query()->latest()->first();
         return Inertia::render('Assessment/SBAView', [
             'levels' => LevelResource::collection(Level::all()),
             'subjects' => SubjectResource::collection(Subject::with('levels')->get()),
-            'students' => StudentResource::collection(Student::where('form', $request->level)
-                ->where('status', Student::ACTIVE)->orderBy('name', 'ASC')->get()),
+            'students' => StudentMarkResource::collection(Student::where('students.form', $request->level)
+                ->leftJoin('marks', function ($join) use ($request, $academic) {
+                    $join->on('students.id', '=', 'marks.student_id')
+                        ->where('marks.subject', $request->subject)
+                        ->where('marks.year', $academic->year)->where('marks.term', $academic->term);
+                })
+                ->select('students.id', 'students.number', 'students.name', 'students.gender',
+                    'marks.year', 'marks.term', 'marks.form', 'marks.subject', 'marks.assessment_one',
+                    'marks.assessment_two', 'marks.assessment_three', 'marks.assessment_four', 'marks.test_one',
+                    'marks.test_two', 'marks.assignment_one', 'marks.assignment_two', 'marks.assignment_three',
+                    'marks.assignment_four', 'marks.exam')->get()
+            ),
         ]);
     }
 
