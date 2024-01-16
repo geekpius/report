22fa5d234dcc2f21f0ee009bff77fe6d0eb6eb50
Fiifi Pius <fiifipius@gmail.com>
2024-01-16 09:15:01 +0000

    student mark resource

---
+++ app/Http/Resources/StudentMarkResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentMarkResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'number' => $this->number,
+            'name' => $this->name,
+            'gender' => $this->gender,
+            'year' => $this->year,
+            'term' => $this->term,
+            'form' => $this->form,
+            'subject' => $this->subject,
+            'assessmentOne' => $this->assessment_one,
+            'assessmentTwo' => $this->assessment_two,
+            'assessmentThree' => $this->assessment_three,
+            'assessmentFour' => $this->assessment_four,
+            'testOne' => $this->test_one,
+            'testTwo' => $this->test_two,
+            'assignmentOne' => $this->assignment_one,
+            'assignmentTwo' => $this->assignment_two,
+            'assignmentThree' => $this->assignment_three,
+            'assignmentFour' => $this->assignment_four,
+            'exam' => $this->exam,
+        ];
+    }
+}
